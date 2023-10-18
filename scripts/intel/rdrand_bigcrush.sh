a51ad8fd1c28a95beb8ad2b7b72be123d7075fac
Jacob Corey <Jacobcorey94@gmail.com>
2023-10-18 17:50:53 -0400

    added i7-7700 tests

--- scripts/intel/rdrand_bigcrush.sh
+++ scripts/intel/rdrand_bigcrush.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 . config.sh
-cd ~
+cd ~/testu01/AMD-TRNG-Testing
 ( ( cat /proc/cpuinfo | grep "model name" ) && ./randTest < run/rdrand_bigcrush.txt ) > intel_rdrand_bigcrush.txt
--- scripts/intel/rdrand_crush.sh
+++ scripts/intel/rdrand_crush.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 . config.sh
-cd ~
+cd ~/testu01/AMD-TRNG-Testing
 ( ( cat /proc/cpuinfo | grep "model name" ) && ./randTest < run/rdrand_crush.txt ) > intel_rdrand_crush.txt
